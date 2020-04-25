00c642e679143d95879de7ed26e968a980ae2c4a
James Patrick Gibson <james@jamespatrickgibson.com>
2020-04-25 14:55:34 -0400

    feat: Hooked up webfonts

--- app/src/components/Header.svelte
+++ app/src/components/Header.svelte
@@ -1,11 +1,27 @@
 <script>
-  import Box from "./Box.svelte";
+
 </script>
 
-<style>
+<style lang="scss">
+  @use "./src/assets/scss/utils/all" as *;
 
+  .header {
+    background: var(--color-neutral-100);
+    height: grid-8pt(7);
+    display: flex;
+    align-items: center;
+    padding: 0 var(--space-2);
+  }
+
+  .logo {
+    font-weight: var(--weight-bold);
+    // text-transform: uppercase;
+    font-size: var(--type-scale-5);
+    // line-height: baseline(6);
+    background: pink;
+  }
 </style>
 
-<div>
-  <Box space="3">Crowdcast</Box>
-</div>
+<header class="header">
+  <div class="logo">Crowdcast</div>
+</header>

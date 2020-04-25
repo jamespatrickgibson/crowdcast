c38ed04fa12054c21a185fd6db0738d1744d9950
Kevin Gaan <kdg49@cornell.edu>
2020-04-25 14:47:08 -0400

    Restructure and return mock data

---
+++ api/constants.py
@@ -0,0 +1,16 @@
+# Query parameters
+QUERY_PARAM_LAT = 'latitude'
+QUERY_PARAM_LONG = 'longitude'
+QUERY_PARAM_RANGE = 'range'
+
+# Response fields
+RESPONSE_ID = 'id'
+RESPONSE_NAME = 'name'
+RESPONSE_LAT = 'latitude'
+RESPONSE_LONG = 'longitude'
+RESPONSE_DIST = 'distance'
+RESPONSE_TIMESTAMP = 'timestamp'
+RESPONSE_MAX_CAP = 'max_capacity'
+RESPONSE_CURR_CAP = 'current_capacity'
+RESPONSE_QUEUE_LEN = 'queue_length'
+RESPONSE_QUEUE_WAIT = 'queue_wait_time'

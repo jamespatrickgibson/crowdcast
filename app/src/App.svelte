f5c88e34cef896e86c27b14e78d6cb3b8b29c1f7
James Patrick Gibson <james@jamespatrickgibson.com>
2020-04-25 12:52:43 -0400

    feat: Hooked up API request

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,3 +1,24 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let venues;
+
+  async function loadVenues() {
+  venues = await fetch("https://nifty-passkey-275314.uc.r.appspot.com/", {
+    "method": "GET",
+      "headers": {
+        "content-type": "application/json"
+      },
+    })
+    .then(response => response.json())
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  onMount(loadVenues);
+</script>
+
 <style lang="scss">
   @use "./src/assets/scss/variables" as *;
 
@@ -6,11 +27,7 @@ color: $color;
 }
 </style>
 
-<script>
-
-</script>
-
 <main>
   <h1>Crowdcast</h1>
-  <div>Venue List</div>
+  <div>{JSON.stringify(venues, 2, null)}</div>
 </main>

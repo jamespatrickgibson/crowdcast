c5cd0edd609a684a64f114b68f8c640f74b78890
Kevin Gaan <kdg49@cornell.edu>
2020-04-25 20:41:22 -0400

    Return some real data

---
+++ api/dao/google.py
@@ -0,0 +1,74 @@
+import json
+
+import requests
+
+from constants import *
+
+
+class GooglePlaces(object):
+    """Fetch nearby places."""
+
+    @staticmethod
+    def get(latitude=DEFAULT_LAT, longitude=DEFAULT_LONG,
+            radius=DEFAULT_RANGE, place_type='bar', open_now=True,
+            page_token=None):
+
+        # Query parameters
+        payload = {
+            'location': f"{latitude},{longitude}",
+            'radius': radius,
+            'type': place_type,
+            'opennow': open_now,
+            'key': API_KEY,
+        }
+
+        # Fetch next page of results
+        if page_token:
+            payload['pagetoken'] = page_token
+
+        req = requests.get(
+            f"{NEARBY_SEARCH_URL}/{NEARBY_SEARCH_OUTPUT}", params=payload
+        )
+
+        return GooglePlaces.parse_response(req.text)
+
+    @staticmethod
+    def parse_response(json_response):
+        response_dict = json.loads(json_response)
+        parsed = {
+            RESPONSE_RESULTS: [],
+            RESPONSE_NEXT_PAGE: response_dict['next_page_token']
+            if 'next_page_token' in response_dict else None,
+        }
+
+        for result in response_dict['results']:
+            parsed[RESPONSE_RESULTS].append({
+                RESPONSE_ID: result['place_id'],
+                RESPONSE_NAME: result['name'],
+                RESPONSE_ADDRESS: result['vicinity'],
+                RESPONSE_LAT: result['geometry']['location']['lat'],
+                RESPONSE_LONG: result['geometry']['location']['lng'],
+                RESPONSE_OPEN: result['opening_hours']['open_now'],
+                RESPONSE_RATING: result['rating'],
+                RESPONSE_PHOTO_URL: GooglePhotos.get_url(
+                    result['photos'][0]['photo_reference']
+                )
+            })
+
+        return parsed
+
+
+class GooglePhotos(object):
+    """Fetch photos (in this context, photos of nearby places."""
+
+    @staticmethod
+    def get_url(photo_reference=None):
+        return f"{PHOTOS_URL}?photoreference={photo_reference}&key={API_KEY}"
+        # # Query parameters
+        # payload = {
+        #     'photoreference': photo_reference,
+        #     'key': API_KEY,
+        # }
+        #
+        # req = requests.get(PHOTOS_URL, params=payload)
+        # return req
--- api/resources/venue.py
+++ api/resources/venue.py
@@ -1,87 +1,46 @@
+import math
 import time
 
 from flask import request
 from flask_restful import Resource
 
 from constants import *
+from dao.google import GooglePlaces
 
 
 class Venue(Resource):
 
     def __init__(self):
         # If location is not specified, default to UC Berkeley
-        self.latitude = 37.8719
-        self.longitude = 122.2585
-        self.range = 10
+        self.latitude = DEFAULT_LAT
+        self.longitude = DEFAULT_LONG
+        self.range = DEFAULT_RANGE
 
     def get(self):
         if self.expected_query_params_exist():
             self.latitude = request.args.get(QUERY_PARAM_LAT)
             self.longitude = request.args.get(QUERY_PARAM_LONG)
-            self.range = request.args.get(QUERY_PARAM_RANGE)
+            # Convert miles to meters
+            self.range = float(request.args.get(QUERY_PARAM_RANGE)) * 1609.34
 
-        return [
-            {
-                RESPONSE_ID: 1,
-                RESPONSE_NAME: "Best bar ever",
-                RESPONSE_LAT: self.latitude,
-                RESPONSE_LONG: self.longitude,
-                RESPONSE_DIST: 0.8,
-                RESPONSE_TIMESTAMP: time.time(),
-                RESPONSE_MAX_CAP: 100,
-                RESPONSE_CURR_CAP: 27,
-                RESPONSE_QUEUE_LEN: 0,
-                RESPONSE_QUEUE_WAIT: 0,
-            },
-            {
-                RESPONSE_ID: 2,
-                RESPONSE_NAME: "In n out",
-                RESPONSE_LAT: self.latitude,
-                RESPONSE_LONG: self.longitude,
-                RESPONSE_DIST: 1.4,
-                RESPONSE_TIMESTAMP: time.time(),
-                RESPONSE_MAX_CAP: 77,
-                RESPONSE_CURR_CAP: 77,
-                RESPONSE_QUEUE_LEN: 10,
-                RESPONSE_QUEUE_WAIT: 8,
-            },
-            {
-                RESPONSE_ID: 3,
-                RESPONSE_NAME: "Another cool bar",
-                RESPONSE_LAT: self.latitude,
-                RESPONSE_LONG: self.longitude,
-                RESPONSE_DIST: 1.6,
-                RESPONSE_TIMESTAMP: time.time(),
-                RESPONSE_MAX_CAP: 85,
-                RESPONSE_CURR_CAP: 80,
-                RESPONSE_QUEUE_LEN: 24,
-                RESPONSE_QUEUE_WAIT: 30,
-            },
-            {
-                RESPONSE_ID: 4,
-                RESPONSE_NAME: "Da club",
-                RESPONSE_LAT: self.latitude,
-                RESPONSE_LONG: self.longitude,
-                RESPONSE_DIST: 1.8,
-                RESPONSE_TIMESTAMP: time.time(),
-                RESPONSE_MAX_CAP: 250,
-                RESPONSE_CURR_CAP: 231,
-                RESPONSE_QUEUE_LEN: 30,
-                RESPONSE_QUEUE_WAIT: 60,
-            },
-            {
-                RESPONSE_ID: 5,
-                RESPONSE_NAME: "Lame bar",
-                RESPONSE_LAT: self.latitude,
-                RESPONSE_LONG: self.longitude,
-                RESPONSE_DIST: 2.5,
-                RESPONSE_TIMESTAMP: time.time(),
-                RESPONSE_MAX_CAP: 10,
-                RESPONSE_CURR_CAP: 1,
-                RESPONSE_QUEUE_LEN: 0,
-                RESPONSE_QUEUE_WAIT: 0,
-            },
-        ]
+        nearby = GooglePlaces.get(self.latitude, self.longitude, self.range,
+                                  open_now=False)
+
+        # Mock the missing data
+        for place in nearby[RESPONSE_RESULTS]:
+            place[RESPONSE_TIMESTAMP] = time.time()
+            place[RESPONSE_DIST] = self.calculate_distance(
+                self.latitude, self.longitude,
+                place[RESPONSE_LAT], place[RESPONSE_LONG]
+            )
+            place[RESPONSE_MAX_CAP] = 100
+            place[RESPONSE_CURR_CAP] = 89
+            place[RESPONSE_QUEUE_LEN] = 10
+            place[RESPONSE_QUEUE_WAIT] = 15
+
+        nearby[RESPONSE_RESULTS] = self.sort(nearby[RESPONSE_RESULTS])
+
+        return nearby
 
     @staticmethod
     def expected_query_params_exist():
@@ -92,3 +51,19 @@ class Venue(Resource):
         expected_params = {QUERY_PARAM_LAT, QUERY_PARAM_LONG, QUERY_PARAM_RANGE}
         diff = expected_params.difference(request.args.keys())
         return len(diff) == 0
+
+    @staticmethod
+    def calculate_distance(lat1, long1, lat2, long2):
+        R = 6371e3
+        phi1 = math.radians(float(lat1))
+        phi2 = math.radians(float(lat2))
+        delta_phi = math.radians(float(lat1) - float(lat2))
+        delta_lambda = math.radians(float(long1) - float(long2))
+        a = math.sin(delta_phi / 2) ** 2 + math.cos(phi1) * math.cos(phi2) * \
+            math.sin(delta_lambda / 2) ** 2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        return R * c / 1609.34  # convert from meters to miles
+
+    @staticmethod
+    def sort(places, sort_field=RESPONSE_DIST, sort_desc=False):
+        return sorted(places, key=lambda i: i[sort_field], reverse=sort_desc)
